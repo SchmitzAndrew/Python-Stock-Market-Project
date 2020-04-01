096fc85ee1f79e41dc8894aa9e6b3d6e2eae7f82
Zeke Egherman <zeke@egherman.com>
2020-04-01 18:14:11 -0500

    add scraping, tqdm

--- get_stock.py
+++ get_stock.py
@@ -1,26 +1,26 @@
-pip install xlrd 
-
 import requests
 import urllib.parse
 import os
 import xlrd
+from tqdm import tqdm
 
-location = ("M:\Programming\Python\Python Stock Market Project\Ticker Symbols.xlsx")
+location = ("./Ticker Symbols.xlsx")
 wb = xlrd.open_workbook(location)
 sheet = wb.sheet_by_index(0)
-sheet.cell value(0,0)
 
-for tickers in range(sheet.nrows):
-    tickers = (sheet.cell_value(i,0))
+tickers = []
+for i in range(sheet.nrows):
+    tickers.append(sheet.cell_value(i,0))
+tickers.pop(0)
+
 
 def get_stock(symbol):
     safe_symbol = urllib.parse.quote(symbol, safe='')
     r = requests.get('https://query1.finance.yahoo.com/v7/finance/download/{}?period1=475804800&period2=1585094400&interval=1d&events=history'.format(safe_symbol), allow_redirects=True)
-    print('downloading data for ' + symbol)
+    # print('downloading data for ' + symbol)
     os.makedirs(os.path.dirname('/data/{}.csv'.format(symbol)), exist_ok=True)
     open('./data/{}.csv'.format(symbol), 'wb').write(r.content)
 
 
-stocks = tickers
-for stock in stocks:
+for stock in tqdm(tickers):
     get_stock(stock)
