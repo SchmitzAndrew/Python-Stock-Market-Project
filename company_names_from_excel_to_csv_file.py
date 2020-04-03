de4f33af2fb35b4b04a17cd415ab65c192a38ee6
Zeke Egherman <zeke@egherman.com>
2020-04-02 23:01:25 -0500

    commit

--- company_names_from_excel_to_csv_file.py
+++ company_names_from_excel_to_csv_file.py
@@ -2,18 +2,14 @@ import os
 import xlrd
 import csv
 
-#opens Excel File
-location = ("./Ticker Symbols.xlsx")
-workbook = xlrd.open_workbook(location)
-sheet = workbook.sheet_by_index(0)
+def get_company_names():
+    #opens Excel File
+    location = ("./Ticker Symbols.xlsx")
+    workbook = xlrd.open_workbook(location)
+    sheet = workbook.sheet_by_index(0)
 
-#Makes List From Excel Column
-company_names = []
-for company_name in range(sheet.nrows):
-    company_names.append(sheet.cell_value(company_name,1))
-company_names.pop(0)
-
-print (company_names)
-
-
-
+    #Makes List From Excel Column
+    stocks = {}
+    for row in range(1,sheet.nrows):
+        stocks[str(sheet.cell_value(row,0))] = sheet.cell_value(row,1)
+    return stocks
